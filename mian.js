f89ae13a487a3e7db75420c0a835ade625cda299
Karol Drożdżalski <46531470+Gluhy@users.noreply.github.com>
2019-06-24 14:59:43 +0100

    now work

--- mian.js
+++ mian.js
@@ -9,46 +9,88 @@ const eight = document.getElementById("eight")
 const nine = document.getElementById("nine")
 const dot = document.getElementById("dot")
 const zero = document.getElementById("zero")
-const rownaSie = document.getElementById("rownaSie")
+const equal = document.getElementById("equal")
 const plus = document.getElementById("plus")
 const minus = document.getElementById("minus")
 const multiply = document.getElementById("multiply")
 const podzielic = document.getElementById("podzielic")
 const clear = document.getElementById("clear")
-const p = document.getElementById("p")
+const p = document.querySelector("p")
 let tablica = ""
 let pamiec = undefined
-
+let operacja = undefined
 
 function buton(event) {
     let element = event.target
+    let wynik = undefined
 
     if (element.value == "="){
-        
+        if (operacja == '-'){
+            wynik = parseInt(pamiec) - parseInt(tablica)
+            tablica = wynik
+            p.innerHTML = wynik
+            operacja = ""
+            }
+        else if (operacja == '+'){
+            wynik = parseInt(pamiec) + parseInt(tablica)
+            tablica = wynik
+            p.innerHTML = wynik
+            operacja = ""
+
+        }
+        else if (operacja == '*'){
+            wynik = parseInt(pamiec) * parseInt(tablica)
+            tablica = wynik
+            p.innerHTML = wynik
+            operacja = ""
+        }
+
+        else if (operacja == '/'){
+            wynik = parseInt(pamiec) / parseInt(tablica)
+            tablica = wynik
+            p.innerHTML = wynik
+            operacja = ""
+        }
     }
 
     else if (element.value == "-"){
+        operacja = '-'
+        pamiec = tablica
+        tablica = ""
+        p.innerHTML = tablica
     }
 
     else if (element.value == "+"){
+        operacja = "+"
+        pamiec = tablica
+        tablica = ""
+        p.innerHTML = tablica
     }
 
     else if (element.value == "*"){
+        operacja = "*"
+        pamiec = tablica
+        tablica = ""
+        p.innerHTML = tablica
     }
 
     else if (element.value == "/"){
-    }
-
-    else if (element.value == "."){
+        operacja = "/"
+        pamiec = tablica
+        tablica = ""
+        p.innerHTML = tablica
     }
 
     else if (element.value == "C"){
+        tablica = ""
+        pamiec = undefined
+        p.innerHTML = tablica
+
     }
 
     else{
         {
             tablica = tablica + element.value
-            console.log(tablica)
             p.innerHTML = tablica
         }
     }
